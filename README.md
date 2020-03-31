8ce18e673313745aa2f01b7e0101db421f399567
RidhwaanDev <ridhwaan.any@gmail.com>
2020-03-31 15:34:33 -0400

    update with map tracking

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
-# Real time Rutgers bus tracking using socket.io 
-client.js and  index.html display a Google map to test the real time bus tracking
+# chat-example
 
-# Running
+This is the source code for a very simple chat example used for
+the [Getting Started](http://socket.io/get-started/chat/) guide
+of the Socket.IO website.
 
-```
-npm start
-open index.html in browser
-```
+Please refer to it to learn how to run this application.
+
+You can also spin up a free Heroku dyno to test it out:
+
+[![Deploy](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy?template=https://github.com/socketio/chat-example)
+
+Besides, the application is deployed on [Now](https://zeit.co/now): https://socketio-chat-example.now.sh/
---
+++ index.js
@@ -0,0 +1,44 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
+const axios = require('axios'); 
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+ 	
+});
+
+io.on('connection', (socket) => {
+  console.log("connected");
+  setInterval(function(){
+   getVehiclesData()
+	.then(result => {
+  	 const response  = result['data'];
+         const data = response['data'];
+         const data_unwrapped = data['1323'];
+         socket.emit('vehicles', data_unwrapped);
+  	})
+  	.catch(error => { console.log(error)});
+  }, 1000);
+});
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
+
+// when the client connects to the backend, make loop to vehicle data 
+
+function getVehiclesData(){
+ const URL = "https://transloc-api-1-2.p.rapidapi.com/vehicles.json";  
+ const h = { headers: {
+     'X-RapidAPI-Host': 'transloc-api-1-2.p.rapidapi.com',
+     'X-RapidAPI-Key':'hHcLr1qWHDmshwibREtIrhryL9bcp1Fw9AQjsnCiZyEzRrJKOS',
+   },
+  params : {
+   'agencies' : '1323' 
+ } 
+};
+ 
+ return axios.get(URL,h);
+}
